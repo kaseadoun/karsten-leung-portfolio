a432d35a16f89ac9b058032d65bb70a50b60175f
Karsten <karstenkleung@gmail.com>
2024-11-14 14:06:04 -0500

    Added prop, classes, and mapped tech stack

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,29 @@
-export default function ProjectItem() {
-    return (
-        <>
-        </>
-    );
+export default function ProjectItem({
+  name,
+  description,
+  techStack,
+  dependencies,
+  url,
+  image,
+}) {
+  return (
+    <div className="project-card">
+      <img className="project-images" src={image} alt={ `Image of ${name}` }/>
+      <div className="project-card-text">
+        <h2>{name}</h2>
+        <p>{description}</p>
+        {url && (
+          <p className="demo-link">
+            <span className="demo">DEMO: </span>
+            <a href={url}>{url}</a>
+          </p>
+        )}
+        <div className="technologies-tag">
+          {techStack.map((tech) => (
+            <span>{tech}</span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
