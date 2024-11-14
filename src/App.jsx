2efcc65812794738fd9d221f996c7952593ebe7e
Karsten <karstenkleung@gmail.com>
2024-11-14 14:05:25 -0500

    Added more content and mapped projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 // Import stylesheet
-import './index.css'
+import "./index.css";
 // Data import
-import projects from "../data/projectData";
+import { projects } from "./data/projectData";
 // Import components
 import Sidebar from "./components/Sidebar";
+import ProjectItem from "./components/ProjectItem";
 
 function App() {
   return (
     <div>
       <Sidebar />
-      <main>
+      <main className="content-container">
         <div id="home">
-
+          <h1>Karsten Leung</h1>
+          <p className="tag">| Software Developer</p>
+          <p className="tag">| Game Dev Hobbyist</p>
         </div>
         <div id="about-me">
-
+          <h2>Who am I?</h2>
         </div>
         <div id="projects">
-
-        </div>
-        <div id="contact-me">
-
+          {
+            projects.map(project => 
+              <ProjectItem 
+                name={ project.name }
+                description={ project.description }
+                techStack={ project.techStack }
+                dependencies={ project.dependencies }
+                url={ project.url }
+                image={ `/project-images/${ project.image }` }
+              />
+            )
+          }
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
