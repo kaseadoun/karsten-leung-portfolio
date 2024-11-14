f161167c1776bdc9892c9f3a1c94218733875a61
Karsten <karstenkleung@gmail.com>
2024-11-14 14:04:22 -0500

    Added data file for navigation to map

---
+++ src/data/navigationData.js
@@ -0,0 +1,14 @@
+export const navigation = [
+    {
+        id: "home",
+        title: "home"
+    },
+    {
+        id: "about-me",
+        title: "about me"
+    },
+    {
+        id: "projects",
+        title: "projects"
+    }
+]
