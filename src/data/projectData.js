1b0a9cbc14d6eb3021e1d105b789ce78abd3c118
Karsten <karstenkleung@gmail.com>
2024-11-13 22:50:12 -0500

    Added some project data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,3 +1,34 @@
 export const projects = [
-    
+    {
+        id: 1,
+        name: "Etch-a-Sketch",
+        description: "",
+        tech: ["HTML", "CSS", "JavaScript"],
+        dependencies: [],
+        url: ""
+    },
+    {
+        id: 2,
+        name: "Island - Console Game",
+        description: "",
+        tech: ["C#"],
+        dependencies: [],
+        url: ""
+    },
+    {
+        id: 3,
+        name: "Weather App",
+        description: "",
+        tech: ["React.js", "CSS"],
+        dependencies: [],
+        url: ""
+    },
+    {
+        id: 4,
+        name: "Portfolio Site",
+        description: "",
+        tech: ["React.js", "CSS"],
+        dependencies: [],
+        url: ""
+    }
 ];
