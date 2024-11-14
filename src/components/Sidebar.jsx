1236a35c9684e61a45c8c27d82ccd6347262b427
Karsten <karstenkleung@gmail.com>
2024-11-13 23:03:30 -0500

    Working on navigation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,30 @@
-import Navigation from "./Navigation";
-import Footer from './Footer';
+// Image imports
+import logo from '../assets/kl-logo.png';
+// Dependency imports
+import { Link } from 'react-scroll';
+// Data import
+import projects from "../data/projectData";
 
+function NavItems() {
+    return(
+        <Link>
+            <li></li>
+        </Link>
+    );
+}
 
 export default function Sidebar() {
     return(
-        <header>
+        <header className="sidebar">
+            <div>
+                <img src={ logo }/>
+            </div>
+            <nav>
 
+            </nav>
+            <footer>
+
+            </footer>
         </header>
     );
 }
