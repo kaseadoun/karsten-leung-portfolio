75af04c401dcdb7e3485c6f66d64913333fae0db
Karsten <karstenkleung@gmail.com>
2024-11-14 23:38:58 -0500

    To upper case all titles for NavItem

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,32 @@
 // Image imports
-import logo from '../assets/kl-logo.png';
+import logo from "../assets/kl-logo.png";
 // Dependency imports
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 // Data imports
-import { navigation } from '../data/navigationData';
+import { navigation } from "../data/navigationData";
 
 function NavItem({ id, title }) {
-    return(
-        <Link to={id}>
-            <li>| { title }</li>
-        </Link>
-    );
+  return (
+    <Link to={id}>
+      <li>| {title.toUpperCase()}</li>
+    </Link>
+  );
 }
 
 export default function Sidebar() {
-    return(
-        <header className="sidebar">
-            <div>
-                <img id="logo" src={ logo }/>
-            </div>
-            <nav>
-                {
-                    navigation.map((navItem) => 
-                        <NavItem 
-                            id={ navItem.id }
-                            title={ navItem.title }
-                        />
-                    )
-                }
-            </nav>
-            <footer>
-
-            </footer>
-        </header>
-    );
+  return (
+    <header className="sidebar">
+      <div>
+        <img id="logo" src={logo} />
+      </div>
+      <nav>
+        <ul>
+          {navigation.map((navItem) => (
+            <NavItem id={navItem.id} title={navItem.title} />
+          ))}
+        </ul>
+      </nav>
+      <footer></footer>
+    </header>
+  );
 }
