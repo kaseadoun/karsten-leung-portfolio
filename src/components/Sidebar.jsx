d2ceab0271c1ca7d268841c80fa5204d4b56b908
Karsten <karstenkleung@gmail.com>
2024-11-15 17:30:30 -0500

    Completed sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,12 @@ import { navigation } from "../data/navigationData";
 
 function NavItem({ id, title }) {
   return (
-    <Link to={id}>
+    <Link
+      to={id}
+      smooth={true}
+      duration={600}
+      {...(id !== "home" && { offset: -50 })}
+    >
       <li>| {title.toUpperCase()}</li>
     </Link>
   );
@@ -15,18 +20,32 @@ function NavItem({ id, title }) {
 
 export default function Sidebar() {
   return (
-    <header className="sidebar">
-      <div>
-        <img id="logo" src={logo} />
-      </div>
-      <nav>
-        <ul>
-          {navigation.map((navItem) => (
-            <NavItem id={navItem.id} title={navItem.title} />
-          ))}
-        </ul>
-      </nav>
-      <footer></footer>
-    </header>
+    <div className="sidebar">
+      <header>
+        <div>
+          <img id="logo" src={logo} />
+        </div>
+        <nav>
+          <ul>
+            {navigation.map((navItem) => (
+              <NavItem id={navItem.id} title={navItem.title} />
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <footer>
+        <a class="footer-links" href="https://github.com/kaseadoun">
+          <i class="fa-brands fa-square-github footer-icons"></i>
+          github.com/kaseadoun
+        </a>
+        <a
+          class="footer-links"
+          href="https://www.linkedin.com/in/karsten-leung/"
+        >
+          <i class="fa-brands fa-linkedin footer-icons"></i>
+          linkedin.com/in/karsten-leung/
+        </a>
+      </footer>
+    </div>
   );
 }
