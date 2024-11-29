6fb59cd733f64c6f2c3a5a1652642b4915cf3a5e
Karsten <karstenkleung@gmail.com>
2024-11-29 17:29:35 -0500

    Added breakpoint conditional rendering and removed useContext

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { projects } from "./data/projectData";
 // Import components
 import Navigation from "./components/Navigation";
 import ProjectItem from "./components/ProjectItem";
+// Import Assets
+// Image imports
+import logo from "./assets/kl-logo.png";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -14,31 +17,47 @@ function App() {
     LARGE_PHONE: 640,
     TABLET: 768,
     LAPTOP: 1024,
-    DESKTOP: 1280
-  }
+    DESKTOP: 1280,
+  };
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
-    }
+    };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div className="app-container">
-      <Navigation 
-        breakpoints={ breakpoints }
-        windowWidth={ windowWidth }
+      <Navigation
+        breakpoints={breakpoints}
+        windowWidth={windowWidth}
+        logoSrc={logo}
       />
       <main className="content-container">
         <section id="home">
-          <h1>Karsten Leung</h1>
-          <div className="tag-container">
-            <p className="tag">| Software Developer</p>
-            <p className="tag">| Game Dev Hobbyist</p>
-          </div>
+          {windowWidth >= breakpoints.TABLET ? (
+            <>
+              <h1>Karsten Leung</h1>
+              <div className="tag-container">
+                <p className="tag">| Software Developer</p>
+                <p className="tag">| Game Dev Hobbyist</p>
+              </div>
+            </>
+          ) : (
+            <div id="home-content">
+              <img id="logo" src={logo} />
+              <div id="home-text-content">
+                <h1>Karsten Leung</h1>
+                <div className="tag-container">
+                  <p className="tag">Software Developer | Game Dev Hobbyist</p>
+                  {/* <p className="tag">| Game Dev Hobbyist</p> */}
+                </div>
+              </div>
+            </div>
+          )}
         </section>
         <section id="about-me">
           <h2>About Me</h2>
@@ -51,8 +70,8 @@ function App() {
             <div className="personal-description-container">
               <h4>Hi, I’m Karsten Leung</h4>
               <p className="personal-description">
-                I'm a software developer who loves building
-                clean, functional, and visually appealing projects.
+                I'm a software developer who loves building clean, functional,
+                and visually appealing projects.
               </p>
               <p className="personal-description">
                 Whether it’s crafting smooth front-end experiences or dabbling
@@ -65,19 +84,23 @@ function App() {
         </section>
         <section id="projects">
           <h2>Projects</h2>
-          {projects.map((project) => (
-            <ProjectItem
-              name={project.name}
-              description={project.description}
-              techStack={project.techStack}
-              dependencies={project.dependencies}
-              url={project.url}
-              image={`/project-images/${project.image}`}
-            />
-          ))}
+          <div className="project-container">
+            {projects.map((project) => (
+              <ProjectItem
+                name={project.name}
+                description={project.description}
+                techStack={project.techStack}
+                dependencies={project.dependencies}
+                url={project.url}
+                image={`/project-images/${project.image}`}
+                breakpoints={breakpoints}
+                windowWidth={windowWidth}
+              />
+            ))}
+          </div>
         </section>
       </main>
-      </div>
+    </div>
   );
 }
 
