69c092d3a56d12aab7193f9079ab26fc2bf46c98
Karsten <karstenkleung@gmail.com>
2025-01-28 22:55:10 -0500

    Added id and key to tech section and removed unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 // Import stylesheet
 import "./index.css";
 // Data import
@@ -10,18 +10,9 @@ import TechIcons from "./components/TechItem";
 // Import Assets
 // Image imports
 import logo from "./assets/kl-logo.png";
-// FontAwesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [activeSection, setActiveSection] = useState("home"); // Track the active section
-  const [sectionIndex, setSectionIndex] = useState(0);
-  const sectionRefs = useRef(["home", "about-me", "projects"]); // Refs for the sections
 
   // Phone dimensions (min-width)
   const breakpoints = {
@@ -40,56 +31,15 @@ function App() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setActiveSection(entry.target.id); // Update active section when it intersects
-          }
-        });
-      },
-      { threshold: 0.7 } // Trigger when the section is fully at the top
-    );
-
-    sectionRefs.current.forEach((ref) => {
-      if (ref) observer.observe(ref);
-    });
-
-    return () => {
-      sectionRefs.current.forEach((ref) => {
-        if (ref) observer.unobserve(ref);
-      });
-    };
-  }, []);
-
-  useEffect(() => {
-    switch (activeSection) {
-      case "home":
-        setSectionIndex(0);
-        break;
-      case "about-me":
-        setSectionIndex(1);
-        break;
-      case "projects":
-        setSectionIndex(2);
-        break;
-      default:
-        return -1; // In case no match is found
-    }
-    console.log(sectionIndex);
-  }, [activeSection]);
-
   return (
     <div className="app-container">
-      {/* <Navigation
-        activeSection={activeSection} // Pass the active section to Navigation
+      <Navigation
         breakpoints={breakpoints}
         windowWidth={windowWidth}
         logoSrc={logo}
-      /> */}
+      />
       <main className="content-container">
-        <section id="home" ref={(el) => (sectionRefs.current[0] = el)} key={0}>
+        <section id="home" key={0}>
           <img id="logo" src={logo} />
           <div id="home-text">
             <h1>KARSTEN LEUNG</h1>
@@ -106,14 +56,13 @@ function App() {
                 className="social-links"
                 href="https://www.linkedin.com/in/karsten-leung/"
               >
-                <i class="fa-brands fa-linkedin social-icons"></i>
+                <i className="fa-brands fa-linkedin social-icons"></i>
               </a>
             </div>
           </div>
         </section>
         <section
           id="about-me"
-          ref={(el) => (sectionRefs.current[1] = el)}
           key={1}
         >
           <h2>ABOUT</h2>
@@ -125,14 +74,16 @@ function App() {
             growing my technical expertise.
           </p>
         </section>
-        <section>
+        <section
+          id="tech-stack"
+          key={2}
+        >
           <h2>TECH STACK</h2>
           <TechIcons />
         </section>
         <section
           id="projects"
-          ref={(el) => (sectionRefs.current[2] = el)}
-          key={2}
+          key={3}
           style={{
             height: "100vh",
           }}
